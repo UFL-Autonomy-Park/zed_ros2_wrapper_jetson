b93c2e6286911b96eb8bf0c6f2ca2458bffd54aa
uflautonomypark <uflautonomypark@gmail.com>
2026-07-13 10:06:39 -0400

    feat: update Dockerfile and scripts to build Dockerfile for the Jetson

--- docker/jetson_build_dockerfile_from_sdk_and_l4T_version.sh
+++ docker/jetson_build_dockerfile_from_sdk_and_l4T_version.sh
@@ -48,7 +48,7 @@ if [ "$(curl -L -I "${CUSTOM_ZED_SDK_URL}" -o /dev/null -s -w '%{http_code}\n' |
 
     echo "Building dockerfile for $1 and a custom ZED SDK"
 
-    docker build -t zed_ros2_l4t_${l4t_major}.${l4t_minor}.${l4t_patch}_sdk_custom \
+    docker build --network=host -t zed_ros2_l4t_${l4t_major}.${l4t_minor}.${l4t_patch}_sdk_custom \
     --build-arg CUSTOM_ZED_SDK_URL=$CUSTOM_ZED_SDK_URL \
     --build-arg L4T_VERSION=$1 \
     --build-arg L4T_MAJOR=$l4t_major \
@@ -70,7 +70,7 @@ else
 
     echo "Building dockerfile for $1 and ZED SDK $2"
 
-    docker build -t zed_ros2_l4t_${l4t_major}.${l4t_minor}.${l4t_patch}_sdk_${sdk_major}.${sdk_minor}.${sdk_patch} \
+    docker build --network=host -t zed_ros2_l4t_${l4t_major}.${l4t_minor}.${l4t_patch}_sdk_${sdk_major}.${sdk_minor}.${sdk_patch} \
     --build-arg ZED_SDK_MAJOR=$sdk_major \
     --build-arg ZED_SDK_MINOR=$sdk_minor \
     --build-arg ZED_SDK_PATCH=$sdk_patch \
--- docker/ros_entrypoint_jetson.sh
+++ docker/ros_entrypoint_jetson.sh
@@ -6,6 +6,8 @@ source "/opt/ros/$ROS_DISTRO/install/setup.bash"
 source "/root/ros2_ws/install/local_setup.bash"
 
 export ROS_DOMAIN_ID=0
+export ROS_LOCALHOST_ONLY=0
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 
 # Welcome information
 echo "ZED ROS2 Docker Image"
@@ -14,6 +16,7 @@ echo 'ROS distro: ' $ROS_DISTRO
 echo 'DDS middleware: ' $RMW_IMPLEMENTATION
 echo 'ROS 2 Workspaces:' $COLCON_PREFIX_PATH
 echo 'ROS 2 Domain ID:' $ROS_DOMAIN_ID
+echo 'ROS 2 localhost Only:' $ROS_LOCALHOST_ONLY
 echo ' * Note: Host and Docker image Domain ID must match to allow communication'
 echo 'Local IPs:' $(hostname -I)
 echo "---"  
@@ -21,6 +24,6 @@ echo 'Available ZED packages:'
 ros2 pkg list | grep zed
 echo "---------------------"
 echo 'To start a ZED camera node:'
-echo '  ros2 launch zed_wrapper zed_camera.launch.py camera_model:=<zed|zedm|zed2|zed2i|zedx|zedxm|zedxonegs|zedxone4k|zedxonehdr|virtual>'
+echo '  ros2 launch zed_wrapper zed_camera.launch.py camera_model:=<zed|zedm|zed2|zed2i|zedx|zedxm|zedxonegs|zedxone4k>'
 echo "---------------------"
 exec "$@"
